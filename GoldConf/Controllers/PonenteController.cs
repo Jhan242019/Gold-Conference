c362554312b2bb4ba4b07951247031c441154877
EdwinVzT12 <70242855+EdwinVzT12@users.noreply.github.com>
2020-10-28 14:57:58 -0500

    Ponente

--- GoldConf/Controllers/PonenteController.cs
+++ GoldConf/Controllers/PonenteController.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GoldConf.Controllers
 {
+    [Authorize]
     public class PonenteController : Controller
     {
+        [HttpGet]
+        public IActionResult Ponente()
+        {
+            return View();
+        }
+        [HttpGet]
         public IActionResult Registrar()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        [HttpGet]
+
+        public IActionResult Eliminar()
         {
             return View();
         }
